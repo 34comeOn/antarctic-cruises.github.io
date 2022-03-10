17379af0c32c4dd997af59e8fc1d6f6ea03700f7
34comeOn <80600393+34comeOn@users.noreply.github.com>
2022-03-10 18:11:34 +0300

    Почти вся валидация и раннее бронирование

--- source/js/main.js
+++ source/js/main.js
@@ -47,6 +47,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
 const mainWrapperElement = document.querySelector('.wrapper');
 const navToggleElement = document.querySelector('.header__toggle');
+const nameInputElement = document.querySelector('.name-input');
+const phoneInputElement = document.querySelector('.phone-input');
+const emailInputElement = document.querySelector('.email-input');
+const formSubmitElement = document.querySelector('.booking__button');
+const checkboxElement = document.querySelector('.booking__checkbox');
+const labelElement = document.querySelector('.booking__label');
 
 mainWrapperElement.classList.remove('wrapper--nojs');
 
@@ -59,3 +65,49 @@ navToggleElement.addEventListener('click', function () {
     mainWrapperElement.classList.remove('wrapper--opened');
   }
 });
+
+const nameInputRe = /[A-Za-zA-Яа-яЁё\s]$/;
+const phoneInputRe = /[0-9]$/;
+const emailInputRe = /\S+@\S+\.\S+/;
+
+const validateInput = (element, re, text) => {
+  element.addEventListener('input', () => {
+    const nameInputArray = element.value.split(' ');
+
+    if (element.value.endsWith(' ')) {
+      nameInputArray.pop();
+    }
+
+    const booleanNameInputArray = nameInputArray.map((nameValidity) =>
+      re.test(nameValidity)
+    );
+
+    nameInputArray.forEach((word) => {
+
+      if (!re.test(word) || booleanNameInputArray.includes(false) || !nameInputElement.value || !phoneInputElement.value || !emailInputElement.value) {
+        formSubmitElement.setAttribute('disabled', 'disabled');
+      } else {
+        formSubmitElement.removeAttribute('disabled');
+      }
+
+      if (!re.test(word)) {
+        element.setCustomValidity(text);
+      } else {
+        element.setCustomValidity('');
+      }
+      element.reportValidity();
+    });
+  });
+};
+
+labelElement.addEventListener('click', function () {
+  if (checkboxElement.hasAttribute('checked')) {
+    checkboxElement.removeAttribute('checked');
+  } else {
+    checkboxElement.setAttribute('checked', 'checked');
+  }
+});
+
+validateInput(nameInputElement, nameInputRe, 'Здесь могут быть только буквы');
+validateInput(phoneInputElement, phoneInputRe, 'Здесь могут быть только цифры');
+validateInput(emailInputElement, emailInputRe, 'Впишите пожалуйста электронный адрес корректно');
